07432e3ee1ef7e60a73ed8d68c8e884f3e187ece
Robert Bovee <Rjbovee112@gmail.com>
2020-02-16 12:52:00 -0700

    added working countdown

--- questions.js
+++ questions.js
@@ -123,3 +123,26 @@ const questions = [
         ]
     }
 ]
+
+//Timer//
+
+function startTimer(duration, display) {
+    var timer = duration, seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 30, 10);
+
+        seconds = seconds < 10 ? + seconds : seconds;
+
+        display.textContent = seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var thirtySeconds = 30 * 5,
+        display = document.querySelector('#timer');
+    startTimer(thirtySeconds, display);
+};
